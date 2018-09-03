c866b95a5e683190e3a48e8cfb2ba29425639951
vskuzmin28 <vskuzmin28@gmail.com>
2018-09-03 14:02:01 +0300

    added links for navigation, fixed nav logic, fixed videgallery

--- pub/js/scripts.js
+++ pub/js/scripts.js
@@ -4,12 +4,24 @@ new WOW().init();
 
 // navigation logic
 
-$('.linksOneSub').hide();
+$('.linksOneSub, .linksTwoSub, .linksThreeSub, .linksFourSub, .linksFiveSub, .linksSixSub').hide();
 
 $('.linkOne').click(function(){
 	event.preventDefault();
 	$('.linksOneSub').show();
 	$('.linkTwo, .linkThree, .linkFour, .linkFive, .linkSix').hide();
+})
+
+$('.linkTwo').click(function(){
+	event.preventDefault();
+	$('.linksTwoSub').show();
+	$('.linkOne, .linkThree, .linkFour, .linkFive, .linkSix').hide();
+})
+
+$('.linkThree').click(function(){
+	event.preventDefault();
+	$('.linksThreeSub').show();
+	$('.linkOne, .linkTwo, .linkFour, .linkFive, .linkSix').hide();
 })
 
 // close sub content
