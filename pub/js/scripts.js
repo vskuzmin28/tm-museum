e44d382e7f0f3f77e744b45e90ea3345a2a224f0
vskuzmin28 <vskuzmin28@gmail.com>
2018-09-06 10:37:19 +0300

    added page photogallery, fixed other pages

--- pub/js/scripts.js
+++ pub/js/scripts.js
@@ -2,9 +2,6 @@
 
 new WOW().init();
 
-// init smooth scroll
-
-var easeInQuad = new SmoothScroll('[data-easing="easeInQuad"]', {easing: 'easeInQuad'});
 
 // navigation logic
 
@@ -34,3 +31,26 @@ $('.linksSubClose').click(function(){
 	$('.linksOneSub, .linksTwoSub, .linksThreeSub, .linksFourSub, .linksOneFive, .linksSixSub').hide();
 	$('.linkOne, .linkTwo, .linkThree, .linkFour, .linkFive, .linkSix').show();
 })
+
+$(".rslides").responsiveSlides({
+	auto: true,             // Boolean: Animate automatically, true or false
+	speed: 500,            // Integer: Speed of the transition, in milliseconds
+	timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+	pager: false,           // Boolean: Show pager, true or false
+	nav: false,             // Boolean: Show navigation, true or false
+	random: false,          // Boolean: Randomize the order of the slides, true or false
+	pause: false,           // Boolean: Pause on hover, true or false
+	pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+	prevText: "Previous",   // String: Text for the "previous" button
+	nextText: "Next",       // String: Text for the "next" button
+	maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+	navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+	manualControls: "",     // Selector: Declare custom pager navigation
+	namespace: "rslides",   // String: Change the default namespace used
+	before: function(){},   // Function: Before callback
+	after: function(){}     // Function: After callback
+  });
+
+  // init smooth scroll
+
+var easeInQuad = new SmoothScroll('[data-easing="easeInQuad"]', {easing: 'easeInQuad'});
